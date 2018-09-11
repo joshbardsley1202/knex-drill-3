ad075316b32310374dab149fab04113c9c1fe97d
joshbardsley1202 <jlbardsley@yahoo.com>
2018-09-11 13:14:36 -0600

    first commit

---
+++ queries.js
@@ -0,0 +1,27 @@
+const database = require("./database-connection");
+
+module.exports = {
+    list(){
+        return database("games")
+    },
+    read(id){
+        return database("games")
+        .where("id",id)
+        .first()
+    },
+    create(games){
+        return database("games")
+        .insert(games, "*")
+        .then(record => record[0])
+    },
+    update(id, games){
+        return database("games")
+        .where("id",id)
+        .update(games, "*")
+    },
+    delete(id){
+        return database("games")
+        .where("id",id)
+        .del()
+    }
+};
---
+++ seeds/01_game.js
@@ -0,0 +1,10 @@
+const games = require("../games")
+
+exports.seed = function(knex, Promise) {
+  return knex("games").del()
+    .then(function () {
+      return knex("games").insert(games);
+    }).then(() => {
+      return knex.raw("ALTER SEQUENCE games_id_seq RESTART WITH 4;")
+    });
+};
